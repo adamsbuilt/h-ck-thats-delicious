7b3354542e270a18877d2d53ea47ed021fcb936e
Austin Adams <austin.atoms@gmail.com>
2017-07-09 23:40:06 -0500

    store model & controller complete-ish

---
+++ models/Store.js
@@ -0,0 +1,107 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: 'Please enter a store name'
+  },
+  slug: String,
+  description: {
+    type: String,
+    trim: true
+  },
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must enter coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must enter an address!'
+    }
+  },
+  photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author'
+  }
+}, {
+  toJSON: {virtuals: true},
+  toObject: {virtuals: true}
+});
+
+// Define our indexes
+storeSchema.index({
+  name: 'text',
+  description: 'text'
+});
+
+storeSchema.index({location: '2dsphere'})
+
+storeSchema.pre('save', async function(next) {
+  if (!this.isModified('name')) {
+    next(); // skip
+    return; // stop this function from running
+  }
+  this.slug = slug(this.name)
+  // find other stores that have a slug multiples
+  const slugRegEx = new RegExp(`^(${this.slug})((-[0-9]*$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({slug: slugRegEx});
+  if(storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
+  }
+  next();
+  // TODO make more resiliant so slugs are unique
+});
+
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    {$unwind: '$tags'},
+    {$group: {_id: '$tags', count: {$sum: 1}}},
+    {$sort: {count: -1}}
+  ]);
+};
+
+storeSchema.statics.getTopStores = function () {
+  return this.aggregate([
+    // Lookup stores and populate their reviews
+    {$lookup: {from: 'reviews', localField: '_id',
+    foreignField: 'store', as: 'reviews'}},
+    // Filter for only items that have 2 or more reviews
+    {$match: {'reviews.1': {$exists: true}}},
+    // And the average reviews field
+    // WHEN USING MONGO 3.4 Use $addField:
+    {$project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      averageRating: {$avg: '$reviews.rating'}
+    }},
+    // Sort by our new field, heighest reviews First
+    {$sort: {averageRating: -1}},
+    // Limit to 10 at most
+    {$limit: 10}
+  ])
+}
+
+// Find reviews where the stores id property === reviews store property
+storeSchema.virtual('reviews', {
+  ref: 'Review', // what model to link??
+  localField: '_id', // which field on the store?
+  foreignField: 'store' // which field on the review?
+});
+
+module.exports = mongoose.model('Store', storeSchema)
